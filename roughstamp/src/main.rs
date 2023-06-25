81075c35ecedecd6b4e04e6c98498694a95a01b5
Peter Todd <pete@petertodd.org>
2023-06-25 14:20:57 -0600

    Add verify command

--- roughstamp/src/main.rs
+++ roughstamp/src/main.rs
@@ -18,10 +18,10 @@
 
 use std::error;
 use std::net::{ToSocketAddrs, UdpSocket};
-use std::io::{self, Write};
+use std::io::{self, Write, Read};
 use std::time::Duration;
 use std::path::PathBuf;
-use std::fs::File;
+use std::fs::{self, File};
 use std::convert::TryInto;
 
 use byteorder::{LittleEndian, ReadBytesExt};
@@ -52,6 +52,10 @@ struct GlobalOpts {
     // /// Verbosity level (can be specified multiple times)
     //#[clap(long, short, global = true, parse(from_occurrences))]
     //verbose: usize,
+
+    /// Use UTC
+    #[clap(long, short, global = true, action)]
+    use_utc: bool,
 }
 
 #[derive(Debug, Subcommand)]
@@ -72,15 +76,14 @@ enum Command {
     },
     /// Verify a timestamp
     Verify {
-        /*
-        /// An example option
-        #[clap(long, short = 'o')]
-        example_opt: bool,
+        /// Expected digest
+        expected_digest: String,
 
-        /// The path to read from
-        path: Utf8PathBuf,
-        // (can #[clap(flatten)] other argument structs here)
-        */
+        /// Timestamp file
+        stamp_file: PathBuf,
+
+        /// Expected server key
+        pub_key: Option<String>,
     },
 }
 
@@ -94,17 +97,15 @@ fn expand_digest(digest: [u8; 32]) -> [u8; 64] {
 }
 
 fn hex_digest_to_digest64(digest: &str) -> Result<Vec<u8>, Box<dyn error::Error>> {
-    let mut digest32: [u8; 32] = HEX.decode(digest.as_bytes())?.try_into().expect("digest not 32 bytes long");
-    let mut digest64 = expand_digest(digest32);
+    let digest32: [u8; 32] = HEX.decode(digest.as_bytes())?.try_into().expect("digest not 32 bytes long");
+    let digest64 = expand_digest(digest32);
     Ok(digest64.into())
 }
 
 fn main() -> Result<(), Box<dyn error::Error>> {
     let cli = Cli::parse();
 
-    dbg!(&cli);
-
-    match cli.command {
+    let resp = match cli.command {
         Command::Stamp { digest, server, pub_key, stamp_file } => {
             let digest = hex_digest_to_digest64(&digest)?;
 
@@ -122,20 +123,45 @@ fn main() -> Result<(), Box<dyn error::Error>> {
 
             let resp_bytes = &buf[0 .. resp_len];
 
-            verify_timestamp(resp_bytes, pub_key, digest)?;
+            let resp = verify_timestamp(resp_bytes, pub_key, digest)?;
 
             let mut fd = File::options().read(true).write(true).create_new(true).open(stamp_file)?;
             fd.write_all(resp_bytes)?;
 
-            Ok(())
+            resp
         },
-        Command::Verify { .. } => {
-            todo!()
+        Command::Verify { expected_digest, stamp_file, pub_key } => {
+            let expected_digest = hex_digest_to_digest64(&expected_digest)?;
+
+            let stamp_bytes = fs::read(stamp_file)?;
+
+            verify_timestamp(&stamp_bytes, pub_key, expected_digest)?
         },
-    }
+    };
+
+    let seconds = resp.midpoint / 10_u64.pow(6);
+    let nsecs = (resp.midpoint - (seconds * 10_u64.pow(6))) * 10_u64.pow(3);
+    let verify_str = if resp.verified { "Yes" } else { "No" };
+
+    let index = -1;
+
+    let midpoint = if cli.global_opts.use_utc {
+        let ts = Utc.timestamp(seconds as i64, nsecs as u32);
+        ts.to_string()
+    } else {
+        let ts = Local.timestamp(seconds as i64, nsecs as u32);
+        ts.to_string()
+    };
+
+    eprintln!(
+        "midpoint={}, radius={:?}, verified={} (merkle_index={})",
+            midpoint, resp.radius, verify_str, index,
+    );
+
+    Ok(())
 }
 
-fn verify_timestamp(raw: &[u8], pub_key: Option<String>, digest: Vec<u8>) -> Result<(), Box<dyn error::Error>> {
+fn verify_timestamp(raw: &[u8], pub_key: Option<String>, digest: Vec<u8>) -> Result<stamp::ParsedResponse, Box<dyn error::Error>> {
     let pub_key = pub_key.map(|pkey| {
         HEX.decode(pkey.as_ref())
            .or_else(|_| BASE64.decode(pkey.as_ref()))
@@ -144,32 +170,6 @@ fn verify_timestamp(raw: &[u8], pub_key: Option<String>, digest: Vec<u8>) -> Res
 
     let resp = RtMessage::from_bytes(raw).unwrap();
 
-    let stamp::ParsedResponse {
-        verified,
-        midpoint,
-        radius,
-    } = dbg!(stamp::ResponseHandler::new(roughenough::version::Version::Classic, pub_key.clone(), resp.clone(), digest)
-        .extract_time());
-
-    let map = resp.into_hash_map();
-    let index = map[&Tag::INDX]
-        .as_slice()
-        .read_u32::<LittleEndian>()
-        .unwrap();
-
-    let seconds = midpoint / 10_u64.pow(6);
-    let nsecs = (midpoint - (seconds * 10_u64.pow(6))) * 10_u64.pow(3);
-    let verify_str = if verified { "Yes" } else { "No" };
-
-    /*
-    let out = if use_utc {
-        let ts = Utc.timestamp(seconds as i64, nsecs as u32);
-        ts.format(time_format).to_string()
-    } else {
-        let ts = Local.timestamp(seconds as i64, nsecs as u32);
-        ts.format(time_format).to_string()
-    };
-    */
-
-    Ok(())
+    Ok(stamp::ResponseHandler::new(roughenough::version::Version::Classic, pub_key.clone(), resp.clone(), digest)
+             .extract_time())
 }
